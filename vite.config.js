09cc555564628b338a847fe1e789bce52d5eeca6
Rohit das <devrohith0@gmail.com>
2026-04-03 15:53:26 +0530

    vite config mm

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' 
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        // This strategy splits your libraries into a separate 'vendor' chunk
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-    // Optional: Increases the warning limit to 1000kB if you're okay with larger chunks
-    chunkSizeWarningLimit: 1000,
-  },
+  plugins: [react(),tailwindcss() ],
 })
